ac72f17d0cae444810a846e8ee3ed850b9f14daf
Olivier <powercat1@gmail.com>
2020-08-27 11:19:50 -0700

    Update ezmt.py

--- ezmt.py
+++ ezmt.py
@@ -6,7 +6,7 @@ import time
 num_threads = 10
 q = Queue()
 
-# A real app wouldn't use hard-coded data.
+#lots of fake data
 items = ['123', '456', '789',
          '012', '345', '678',
          '901', '234', '567',
@@ -49,7 +49,8 @@ items = ['123', '456', '789',
          '789', '012', '345',
          '678', '901', '234',
          '567', '890', '123',]
-         
+
+#message format
 def message(s):
     print('{}: {}'.format(threading.current_thread().name, s))
 
@@ -62,13 +63,13 @@ def worker_process(q):
     the main thread ends.
     """
     while True:
-        #message('looking for the next enclosure')
+        #message('Processing next item')
         item = q.get()
         print(item)
         time.sleep(1) #a little sleep to simulate delay
         q.task_done()
-#Once the target function for the threads is defined, the worker threads can be started. When worker_process() processes the statement url = q.get(), it blocks and waits until the queue has something to return. That means it is safe to start the threads before there is anything in the queue.
 
+#Once the target function for the threads is defined, the worker threads can be started. When worker_process() processes the statement item = q.get(), it blocks and waits until the queue has something to return. That means it is safe to start the threads before there is anything in the queue.
 # Set up some threads to fetch the data.
 for i in range(num_threads):
     worker = threading.Thread(
@@ -78,16 +79,14 @@ for i in range(num_threads):
     )
     worker.setDaemon(True)
     worker.start()
-#The next step is to retrieve the feed contents using the feedparser module and enqueue the URLs of the enclosures. As soon as the first URL is added to the queue, one of the worker threads picks it up and starts downloading it. The loop continues to add items until the feed is exhausted, and the worker threads take turns dequeuing URLs to download them.
 
-# Download the feed(s) and put the enclosure URLs into
-# the queue.
+#below is the code that'll fill the queue with the items to process
 for item in items:
     q.put(item)
-#The only thing left to do is wait for the queue to empty out again, using join().
 
+#The only thing left to do is wait for the queue to empty out again, using join().
 # Now wait for the queue to be empty, indicating that we have
-# processed all of the downloads.
+# processed all of the data.
 message('*** main thread waiting')
 q.join()
 message('*** done')
